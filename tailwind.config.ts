0a5e9fa82123ba29b34282263ede20f35454b74e
Ashutosh Raj Gupta <ag2364443@gmail.com>
2024-07-17 14:09:56 +0530

    changes done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,63 @@
+// import type { Config } from "tailwindcss";
+
+// const {
+//   default: flattenColorPalette,
+// } = require("tailwindcss/lib/util/flattenColorPalette");
+
+// const config: Config = {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+
+//       fontFamily: {
+//         poppins: ['Poppins', 'sans-serif'],
+//       },
+//       animation: {
+//         spotlight: "spotlight 2s ease .75s 1 forwards",
+//       },
+//       keyframes: {
+//         spotlight: {
+//           "0%": {
+//             opacity: 0,
+//             transform: "translate(-72%, -62%) scale(0.5)",
+//           },
+//           "100%": {
+//             opacity: 1,
+//             transform: "translate(-50%,-40%) scale(1)",
+//           },
+//         },
+//       },
+//       boxShadow: {
+//         input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+//       },
+//       backgroundImage: {
+//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+//         "gradient-conic":
+//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+//       },
+//     },
+//   },
+//   plugins: [addVariablesForColors],
+// };
+// export default config;
+
+
+
+// function addVariablesForColors({ addBase, theme }: any) {
+//   let allColors = flattenColorPalette(theme("colors"));
+//   let newVars = Object.fromEntries(
+//     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+//   );
+
+//   addBase({
+//     ":root": newVars,
+//   });
+// }
+
 import type { Config } from "tailwindcss";
 
 const {
@@ -12,7 +72,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
@@ -22,11 +81,11 @@ const config: Config = {
       keyframes: {
         spotlight: {
           "0%": {
-            opacity: 0,
+            opacity: "0",
             transform: "translate(-72%, -62%) scale(0.5)",
           },
           "100%": {
-            opacity: 1,
+            opacity: "1",
             transform: "translate(-50%,-40%) scale(1)",
           },
         },
@@ -43,9 +102,8 @@ const config: Config = {
   },
   plugins: [addVariablesForColors],
 };
+
 export default config;
-
-
 
 function addVariablesForColors({ addBase, theme }: any) {
   let allColors = flattenColorPalette(theme("colors"));
